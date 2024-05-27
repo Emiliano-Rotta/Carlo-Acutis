c1ce56dc94e1d7c5bf07dc6824cff02fb57d986c
Emiliano Rotta <emilianorottaf@gmail.com>
2024-05-27 13:17:43 -0300

    Ruta milagro E

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import AparicionesM from './pages/AparicionesM'; 
 import Santos from './pages/Santos'; 
 import Vida from './pages/Vida'; 
+import Milagros from './pages/Milagros'; 
 import NavBar from './view/NavBar'; 
 import Footer from './view/Footer'; 
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/apariciones-marianas" element={<AparicionesM />} />
           <Route path="/santos" element={<Santos />} />
           <Route path="/vida" element={<Vida />} />
+          <Route path="/milagros-eucaristicos" element={<Milagros />} />
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
         <Footer />

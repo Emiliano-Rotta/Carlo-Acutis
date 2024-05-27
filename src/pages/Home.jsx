244cc6eb2c1f8809de23eaccdfc37e5d9a120848
Emiliano Rotta <emilianorottaf@gmail.com>
2024-05-27 13:14:21 -0300

    img Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-function Home() {
+import remeraroja from "../assets/img/carlo/remeraroja.jpeg";
+import Figure from 'react-bootstrap/Figure';
 
-    return (
-      <>
-        esto es Home
-      </>
-    )
-  }
-  
-  export default Home
+function Home() {
+  return (
+    <Figure>
+      <Figure.Image
+        width={"100%"}
+        // height={180}
+        alt=""
+        src={remeraroja}
+      />
+      <Figure.Caption>
+        Nulla vitae elit libero, a pharetra augue mollis interdum.
+      </Figure.Caption>
+    </Figure>
+  );
+}
+
+export default Home;
